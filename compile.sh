01abe14f5302f540088edc70287f3b65e276de5b
hikaruaikawa <hikaruaikawa@gmail.com>
2017-10-11 16:26:13 +0200

    Removed dependency from MinGW libraries in executable

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -DSFML_STATIC main.cpp -I./include -L./lib -lsfml-graphics-s -lfreetype -ljpeg -lsfml-window-s -lgdi32 -lopengl32 -lsfml-network-s -lws2_32 -lsfml-system-s -lwinmm -o cubetac
+g++ -DSFML_STATIC main.cpp -I./include -L./lib -lsfml-graphics-s -lfreetype -ljpeg -lsfml-window-s -lgdi32 -lopengl32 -lsfml-network-s -lws2_32 -lsfml-system-s -lwinmm -static-libgcc -static-libstdc++ -o cubetac
